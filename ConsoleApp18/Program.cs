7c9c41cbb4ef2b08d188634255b0a814861c4a34
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2024-01-08 12:50:49 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,9 @@
+﻿try
+{
+    string[] d = args[0].Split('-');
+    Console.WriteLine(new DateOnly(Int32.Parse(d[2]), Int32.Parse(d[0]), int.Parse(d[1])).ToString("yyyy/MM/dd"));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
